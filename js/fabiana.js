c6b7208cf134ad1dbb4e09d176c3a3d6ac17ac62
Horácio J <lanterna_@hotmail.com>
2017-08-05 01:46:52 -0300

    mais angular salvo

--- js/fabiana.js
+++ js/fabiana.js
@@ -1,12 +1,16 @@
 angular.module('myApp').controller('fabiana',['$scope', function($scope){
         
-        
-        
         $scope.nome="fabiana";
         $scope.idade="36";
         $scope.info=["trabalho","diversao","show do roupa nova"];
+        $scope.objeto="tt";
         $scope.dados={"documentos":["rg","cpf","cnh"," bluehost","bb"],"roupas":[1,2,3,4,5]};
         
+        $scope.objeto="r";
+        $scope.Carrega=function(){
+            //alert("teste"); 
+            $scope.dados.documentos.push($scope.objeto);
+            $scope.objeto="";
+        };
         
-        
-}]);
+}]); 
